27ad926ad0bdbdbeeb52901c1ea83dc5e5051ee1
Sahil Chachra <37156032+SahilChachra@users.noreply.github.com>
2020-06-12 20:15:34 +0530

    Changing version number to 0.3. Added Dynamic features

--- Screenshot.py
+++ Screenshot.py
@@ -15,8 +15,6 @@ dir = input("Destination : ")
 #++++++++++++++++++++
 #----------TO ADD---------
 # Fix - Exiting program
-# Add - option to given user defined key combinations or else user pre defined key combinations
-# Add - cutom time interval
 
 #+++++++++++++++++++++++
 
@@ -27,14 +25,39 @@ dir = input("Destination : ")
 # 2. Can take Single Shots and save directly instead explicitly opening another app to save it
 # 3. Can store data in user defined directory
 # 4. Name of file is current Date-Time to easily sort data for editing purpose
-# 5. 
+# 5. User has the option to either use their own settings or default settings
+# 6. User can set custom time interval for taking timelapse
 
 # - THE PYTHON SCRIPT SHOULD BE RUNNING IN BACKGROUND TO DO IT'S WORK PEACEFULLY!
+
+# Creating settings (default/user defined)
+
+def setKeyCombination():
+    global keyToStartTimelapse, keyToEndTimelapse, keyToTakeSingleShot, keyToEndSingleShot
+    print("Do you want to use Default key combinations or Set your own?")
+    keyChoice = int(input("Enter 0 for DEFAULT and 1 for CUSTOM : "))
+    while True:
+        if(keyChoice == 0):
+            keyToStartTimelapse =  "shift + space"
+            keyToEndTimelapse = "" #NOT IMPLEMENTED YET
+            keyToTakeSingleShot = "shift + alt"
+            keyToEndSingleShot = "shift + `"
+            break
+        elif(keyChoice == 1):
+            keyToStartTimelapse = input("Enter key combination to START Timelapse Shots : ")
+            keyToEndTimelapse = input("Enter key combination to END Timelapse Shots (YET TO BE IMPLEMENTED): ")
+            keyToTakeSingleShot = input("Enter key combination to START Single Shots : ")
+            keyToEndSingleShot = input("Enter key combination to END Single Shots : ")
+            break
+        else:
+            print("Enter valid choice!")
+
 
 # FOR TIMELAPSE PURPOSE
 def timelapse():
     while True:
-        if keyboard.is_pressed("shift + space"): #The keycombination should be such that you don't press it by mistake!
+        timelapseDelay = int(input("Enter delay between two Screenshot : "))
+        if keyboard.is_pressed(keyToStartTimelapse): #The keycombination should be such that you don't press it by mistake!
             while True:
                 #if keyboard.is_pressed("shift + q"):
                 #break
@@ -44,7 +67,7 @@ def timelapse():
                 dest = dir + currTime + '.png'
                 ss.save(dest)
                 dest = ""
-                time.sleep(5)
+                time.sleep(timelapseDelay)
             break
         quit()
 
@@ -55,27 +78,31 @@ def timelapse():
 
 def singleShots():
     while True:
-        if keyboard.is_pressed("shift + alt"): # To trigger single shots
+        if keyboard.is_pressed(keyToTakeSingleShot): # To trigger single shots
             ss = pyautogui.screenshot()
             currTime = time.asctime(time.localtime()).replace(" ","-").replace(":","+")
             dest = dir + currTime + '.png'
             ss.save(dest)
             dest = ""
-        elif keyboard.is_pressed("shift + `"):
+        elif keyboard.is_pressed(keyToEndSingleShot):
             break
 
 while True:
+    
     print("1. Time Lapse\n2. Single Shots\n3. Exit\n")
     choice = int(input("Enter Choice : "))
 
     if(choice == 1):
+        setKeyCombination()
         timelapse()
         #quit()
     elif(choice == 2):
+        setKeyCombination()
         singleShots()
     elif(choice == 3):
         quit()
     else:
         print("Invalid Choice!\n")
+
 
 
